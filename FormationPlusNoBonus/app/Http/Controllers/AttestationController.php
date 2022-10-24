50a0a72738355de3ca75661d4bf6e8069af0ad1f
Rioc <rio.clement77@gmail.com>
2022-10-24 16:23:26 +0200

    premier push + création des migrations, créations d'un dossier sans bonus

---
+++ FormationPlusNoBonus/app/Http/Controllers/AttestationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Attestation;
+use Illuminate\Http\Request;
+
+class AttestationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Attestation  $attestation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attestation $attestation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Attestation  $attestation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Attestation $attestation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Attestation  $attestation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Attestation $attestation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Attestation  $attestation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attestation $attestation)
+    {
+        //
+    }
+}
